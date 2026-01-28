77e30ac80aa07dd1bb264124d2ed778dfe7fa1c9
goldipl <mgodlewski92@gmail.com>
2026-01-28 20:27:10 +0100

    add routing and page components for homepage, category, and review details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+
+import SiteHeader from './components/SiteHeader';
+
+import Homepage from './pages/Homepage';
+import Category from './pages/Category';
+import ReviewDetails from './pages/ReviewDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <main>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/details/:id" element={<ReviewDetails />} />
+            <Route path="/category/:id" element={<Category />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
